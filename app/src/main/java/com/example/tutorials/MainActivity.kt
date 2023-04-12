899d174dcb2633422364433949b8b06c2a0b3d24
Ahmed ramadan <ahmedtramadan4@gmail.com>
2023-04-12 09:48:27 +0200

    IMPLICIT INTENTS

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,63 +1,44 @@
 package com.example.tutorials
 
-import android.Manifest
-import android.content.pm.PackageManager
+
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import androidx.core.app.ActivityCompat
-
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btnRequest = findViewById<Button>(R.id.btnReqPermission)
-        btnRequest.setOnClickListener{requestPermissions()}
 
-    }
+        var takePhoto: Button = findViewById(R.id.btnTakePhoto)
+        var photoImg: ImageView = findViewById(R.id.ivPhoto)
 
-    private fun hasWriteExternalStoragePermission() =
-        ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-
-    private fun hasLocationForegroundPermission() =
-        ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-
-    private fun hasLocationBackgroundPermission() =
-        ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-
-
-    private fun requestPermissions(){
-        var permissionsToRequest = mutableListOf<String>()
-        if (!hasWriteExternalStoragePermission()){
-            permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        }
-        if (!hasLocationForegroundPermission()){
-            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-        if (!hasLocationBackgroundPermission()){
-            permissionsToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-        }
-        if (permissionsToRequest.isNotEmpty()){
-            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 0 && grantResults.isNotEmpty()){
-            for (i in grantResults.indices) {
-                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
-                    Log.d("PermissionsRequest", "${permissions[i]} granted")
-                }
+        takePhoto.setOnClickListener{
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it,0)
             }
         }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 0){
+            var photoImg: ImageView = findViewById(R.id.ivPhoto)
+            val uri = data?.data
+            photoImg.setImageURI(uri)
+        }
+
+    }
+
 
 }
